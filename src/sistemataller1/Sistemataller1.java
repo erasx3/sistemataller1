649674ba0512f94c01337b27a6b9f98fa4259b81
erasx3 <erasx3@gmail.com>
2015-06-11 00:41:26 +0200

    1-inicio proytecto

---
+++ src/sistemataller1/Sistemataller1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemataller1;
+
+/**
+ *
+ * @author eras
+ */
+public class Sistemataller1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
