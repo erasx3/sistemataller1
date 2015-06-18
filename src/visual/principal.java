f68b382083dc2c3b37e61107650f3c8684672025
erasx3 <erasx3@gmail.com>
2015-06-18 23:30:46 +0200

    12-mierda pura mierda

--- src/visual/principal.java
+++ src/visual/principal.java
@@ -20,23 +20,23 @@ public class principal extends javax.swing.JFrame {
     /**
      * Creates new form principal
      */
-//     public static void main(String[] args) 
-//   {
-//      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "sistemataller1PU" );
-//      EntityManager entitymanager = emfactory.createEntityManager( );
-//      entitymanager.getTransaction( ).begin( );
-//
-//      //Create Clas Entity
-//      Provincia clas1=new Provincia("misiones");
-//      Provincia clas2=new Provincia("catamarca");
-//      Provincia clas3=new Provincia("entre rios");
-//
-//      //Store Clas
-//      entitymanager.persist(clas1);
-//      entitymanager.persist(clas2);
-//      entitymanager.persist(clas3);
-//      
-//   }
+     public static void main(String[] args) 
+   {
+      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "sistemataller1PU" );
+      EntityManager entitymanager = emfactory.createEntityManager( );
+      entitymanager.getTransaction( ).begin( );
+
+      //Create Clas Entity
+      Provincia clas1=new Provincia("misiones");
+      Provincia clas2=new Provincia("catamarca");
+      Provincia clas3=new Provincia("entre rios");
+
+      //Store Clas
+      entitymanager.persist(clas1);
+      entitymanager.persist(clas2);
+      entitymanager.persist(clas3);
+      
+   }
     public principal() {
         initComponents();
     }
@@ -180,37 +180,37 @@ public class principal extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new principal().setVisible(true);
-            }
-        });
-    }
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new principal().setVisible(true);
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu2;
